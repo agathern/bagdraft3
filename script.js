6bfa8d093e109e06dac188f4eac57da2d305398f
agathern <72084016+agathern@users.noreply.github.com>
2021-04-20 10:56:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ var base = new Airtable({apiKey: 'keyYYTnth0cc4iU3Z'}).base('apps0SfuzuzykUNph')
 
 //get our collection base select all the records
 //specify functions
-base("bags").select({}).eachPage(gotPageOfBags, gotAllBags);
+base("bags")
+  .select({})
+  .eachPage(gotPageOfBags, gotAllBags);
 
 // an empty array to hold our data
 var bags = [];
@@ -45,7 +47,7 @@ function gotAllBags(err) {
 // just loop through the bags and console.log them
 function consoleLogbags() {
   console.log("consoleLogbags()");
-  bags.forEach((bag) => {
+  bags.forEach(bag => {
     console.log("bag:", bag);
   });
 }
@@ -53,20 +55,10 @@ function consoleLogbags() {
 // loop through the bags, create an h2 for each one, and add it to the page
 function showbags() {
   console.log("showbags()");
-  bags.forEach((bag) => {
+  bags.forEach(bag => {
 
-    //var bagTitle = document.createElement("h1");
-   // bagTitle.innerText = bag.fields.bag;
-    //document.body.append(bagTitle);
-
-    //var bagSize = document.createElement("p");
-    //bagSize.innerText = bag.fields.size;
-    //document.body.append(bagSize);
-
-    //var bagImage = document.createElement("img");
-    //bagImage.src = bag.fields.image[0].url;
-   // document.body.append(bagImage);
-
+    
+  
    var bagContainer = document.createElement("div");
    bagContainer.classList.add("bag-container");
    document.querySelector(".container").append(bagContainer);
@@ -75,11 +67,11 @@ function showbags() {
     bagTitle.classList.add("bag-title");
     bagTitle.innerText = bag.fields.bag;
     bagContainer.append(bagTitle);
-
-  // var bagSize = document.createElement("h1")
-   // bagSize.classList.add("bag-title");
-    // bagSize.innerText = bag.fields.size;
-   //  bagContainer.append(bagSize);
+    
+  var bagSize = document.createElement("h1");
+    bagSize.classList.add("bag-size");
+    bagSize.innerText = bag.fields.size;
+    bagContainer.append(bagSize);
 
     var bagDescription = document.createElement("p");
     bagDescription.classList.add("bag-description");
@@ -90,24 +82,49 @@ function showbags() {
     bagImage.classList.add("bag-image");
     bagImage.src = bag.fields.image[0].url;
     bagContainer.append(bagImage); 
+    
+              
+    var bagSize = bag.fields.size;
+    bagContainer.classList.add(bagSize);
 
-    bagContainer.addEventListener("click", function(){
+    bagContainer.addEventListener("click", function(event){
       bagDescription.classList.toggle("active");
       bagTitle.classList.toggle("active");
       bagContainer.classList.toggle("active");
       bagImage.classList.toggle("active");
-     })
-     });
+    
+    });
+    
+   
+      // get genre field from airtable
+    // loop through the array and add each genre as
+    // a class to the bag container
+    
+    // var bagSize = bag.fields.size;
+    //  bagSize.forEach(function(size){
+    //   bagContainer.classList.add(size);  
+    // });
 
-
-  var bagContainer = bag.fields.size;
-    bagContainer.forEach(function(size) {
-      bagContainer.classList.add(size);
+  // var bagSize = bag.fields.size;
+  //   bagContainer.forEach(function(size){
+  //       bagContainer.classList.add(size);
+  //   })
+  
+     
+    // clicking on filter by x-small
+    // change display
+    var filterXSmall = document.querySelector(".js-x-small");
+    filterXSmall.addEventListener("click", function() {
+      if (bagContainer.classList.contains("x-small")) {
+        bagContainer.style.display = "block";
+      } else {
+        bagContainer.style.display = "none";
+      }
     });
 
-  
+    // filter by small
     var filterSmall = document.querySelector(".js-small");
-    filterPop.addEventListener("click", function() {
+    filterSmall.addEventListener("click", function() {
       if (bagContainer.classList.contains("small")) {
         bagContainer.style.display = "block";
       } else {
@@ -115,8 +132,45 @@ function showbags() {
       }
     });
 
+    // filter by medium
+    var filterMedium = document.querySelector(".js-medium");
+    filterMedium.addEventListener("click", function() {
+      if (bagContainer.classList.contains("medium")) {
+        bagContainer.style.display = "block";
+      } else {
+        bagContainer.style.display = "none";
+      }
+    });
 
+    // filter by large
+    var filterLarge = document.querySelector(".js-large");
 
+    filterLarge.addEventListener("click", function() {
+      if (bagContainer.classList.contains("large")) {
+             bagContainer.style.display = "block";
+      } else {
+        bagContainer.style.display = "none";
+      }
+    });
 
+    // filter by x-large
+    var filterXLarge = document.querySelector(".js-x-large");
+
+    filterXLarge.addEventListener("click", function() {
+      if (bagContainer.classList.contains("x-large")) {
+             bagContainer.style.display = "block";
+      } else {
+        bagContainer.style.display = "none";
+      }
+    });
+
+    // filter reset
+    var filterReset = document.querySelector(".js-reset");
+    filterReset.addEventListener("click", function() {
+      bagContainer.style.display = "block";
+    });
+  });
+  
 }
+
 
